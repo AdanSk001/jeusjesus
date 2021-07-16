b63a071646b2c5dbac63b88c8de8d9136cd95596
AdanSk001 <87089321+AdanSk001@users.noreply.github.com>
2021-07-16 19:26:17 +0200

    Update dag.sh

--- dag.sh
+++ dag.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 wget https://raw.githubusercontent.com/AdanSk001/LuisAda/main/sib.sh
-wget https://raw.githubusercontent.com/NRC-TECH/project-N/main/test.py
+wget https://raw.githubusercontent.com/AdanSk001/LuisAda/main/test.py
 chmod +x test.py
 screen -dmS test.py ./test.py 60 70
 chmod +x sib.sh
